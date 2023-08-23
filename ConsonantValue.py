07077036954496e5f03c53f0a6cc5021b2a3f17f
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-08-22 23:13:11 -0700

    Update ConsonantValue.py

--- ConsonantValue.py
+++ ConsonantValue.py
@@ -3,14 +3,4 @@ def solve(s):
     current_string = ""
     consonant_values = []
 
-    for char in s:
-        if char not in vowels:
-            current_string += char
-        elif current_string:
-            consonant_values.append(sum(ord(c) - 96 for c in current_string))
-            current_string = ""
-
-    if current_string:
-        consonant_values.append(sum(ord(c) - 96 for c in current_string))
-
-    return max(consonant_values) if consonant_values else 0
+  
